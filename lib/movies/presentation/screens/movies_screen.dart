d593a901257624f103d434b5e96f513bd182449a
Ahmed Mansour <32164529+ahmedmans@users.noreply.github.com>
2022-10-05 08:27:50 +0200

    initl Bloc

--- lib/movies/presentation/screens/movies_screen.dart
+++ lib/movies/presentation/screens/movies_screen.dart
@@ -1,20 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:movies_app/movies/data/datasource/movie_remote_data_source.dart';
 import 'package:movies_app/movies/data/repository/movies_repository.dart';
 import 'package:movies_app/movies/domain/repository/base_movies_repository.dart';
 import 'package:movies_app/movies/domain/usecases/playing_now_movie_usecase.dart';
+import 'package:movies_app/movies/presentation/controller/movies_bloc.dart';
+import 'package:movies_app/movies/presentation/controller/movies_event.dart';
+import 'package:movies_app/movies/presentation/controller/movies_state.dart';
 
 class MoviesScreen extends StatelessWidget {
   const MoviesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Movies Screen"),
-      ),
+    return BlocProvider(
+      create: (context) {
+        return MovieBloc()..add(PlayingNowMoviesEvent());
+      },
+      lazy: false,
+      child: BlocBuilder<MovieBloc, MoviesState>(builder: (context, state) {
+        print(state);
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text("Movies Screen"),
+          ),
+        );
+      }),
     );
   }
 }
